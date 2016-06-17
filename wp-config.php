3038e719e73bf3cd27d67955e243f618c562c449
namwoody <deepsarora262@gmail.com>
2016-06-16 23:47:08 -0400

    done

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'kart');
+define('DB_NAME', 'kartzill');
 
 /** MySQL database username */
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'D7Hpk2lRsx');
 
 /** MySQL hostname */
 define('DB_HOST', '127.0.0.1');
